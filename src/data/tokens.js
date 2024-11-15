edb5756c1f5c9b2d0c3b9d1c190fd70b491f02e2
yaxyobekb <pubgn9642@gmail.com>
2024-11-15 15:15:36 +0500

    add tokens data

---
+++ src/data/tokens.js
@@ -0,0 +1,38 @@
+import xIcon from "../assets/images/tokens/x.svg";
+import dogsIcon from "../assets/images/tokens/dogs.svg";
+import hmstrIcon from "../assets/images/tokens/hmstr.png";
+import notIcon from "../assets/images/tokens/notcoin.svg";
+import toncoinIcon from "../assets/images/tokens/toncoin.svg";
+
+export default [
+  {
+    name: "ton",
+    price: 63000,
+    icon: toncoinIcon,
+    description: null,
+  },
+  {
+    name: "x",
+    price: 3.5,
+    icon: xIcon,
+    description: "Topda",
+  },
+  {
+    name: "not",
+    price: 90,
+    icon: notIcon,
+    description: "Eng yaxshisi",
+  },
+  {
+    name: "dogs",
+    price: 7.5,
+    icon: dogsIcon,
+    description: null,
+  },
+  {
+    name: "hmstr",
+    price: 35,
+    icon: hmstrIcon,
+    description: null,
+  },
+];
