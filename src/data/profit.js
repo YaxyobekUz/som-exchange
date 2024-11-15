c3f09cc63efabc98fdd52b1edf5de3e052c455c2
yaxyobekb <pubgn9642@gmail.com>
2024-11-15 15:15:28 +0500

    add profit data

---
+++ src/data/profit.js
@@ -0,0 +1,46 @@
+export default {
+  min: 25000,
+  max: 999999999999999,
+  prices: [
+    {
+      from: 0,
+      to: 50000,
+      percentage: 19,
+    },
+    {
+      from: 50000,
+      to: 200000,
+      percentage: 18,
+    },
+    {
+      from: 500000,
+      to: 1000000,
+      percentage: 17,
+    },
+    {
+      from: 1000000,
+      to: 3000000,
+      percentage: 16,
+    },
+    {
+      from: 3000000,
+      to: 5000000,
+      percentage: 15,
+    },
+    {
+      from: 5000000,
+      to: 10000000,
+      percentage: 13.5,
+    },
+    {
+      from: 10000000,
+      to: 20000000,
+      percentage: 12,
+    },
+    {
+      from: 20000000,
+      to: 999999999999999,
+      percentage: 10,
+    },
+  ],
+};
