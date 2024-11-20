a3885476fe58c440f441fff698f84855b0308ed2
yaxyobekb <pubgn9642@gmail.com>
2024-11-20 20:15:37 +0500

    add gradient colors data

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,7 @@
+import gradientColors from "./data/gradientColors";
+
 export const getFirstLetter = (text) => (text ? text[0] : undefined);
+export const getRandomGradientColor = () => {
+  const randomIndex = Math.floor(Math.random() * gradientColors.length);
+  return gradientColors[randomIndex];
+};
